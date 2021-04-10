160cd99d0e61a87122b5234b4091ee4bc01213d1
William Angulo <wmavalle@gmail.com>
2021-04-10 00:28:39 -0500

    "fetching-correct"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import './App.css';
-import './components/Post'
-import Post from './components/Post';
-function App() {
-  return (
-    <div className="App">
-      <Post></Post>
-    </div>
-  );
+import React, { Component } from 'react';
+import Posts from './containers/Posts';
+import styled from 'styled-components';
+const Container = styled.div`
+  max-width: 980px;
+  margin: 0 auto;
+`;
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <h1>React blog</h1>
+        <h2>Posts: </h2>
+        <Posts />
+      </Container>
+    );
+  }
 }
 
 export default App;
--- src/utils/api.js
+++ src/utils/api.js
@@ -3,3 +3,13 @@ import axios from "axios";
 export function getPosts() {
     return axios.get('https://jsonplaceholder.typicode.com/posts');
 }
+
+axios({
+    method:'post',
+    url: 'https://jsonplaceholder.typicode.com/posts',
+    data: {
+        id: '',
+        title:'',
+        body:''
+    }
+})
